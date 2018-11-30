bfc4b5cd4911a500d8c8a1db0a66ecb32eb1468c
岁月如歌歌歌 <javaEEzhang@aliyun.com>
2018-11-30 10:09:09 +0800

    断路器监控

--- service-hi/src/main/java/com/zw/servicehi/controller/IndexController.java
+++ service-hi/src/main/java/com/zw/servicehi/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.zw.servicehi.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ public class IndexController {
     private RestTemplate restTemplate;
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String callHome(){
         LOG.log(Level.INFO, "calling trace service-hi ");
         return restTemplate.getForObject("http://localhost:8989/miya", String.class);
--- service-miya/src/main/java/com/zw/servicemiya/ServiceMiyaApplication.java
+++ service-miya/src/main/java/com/zw/servicemiya/ServiceMiyaApplication.java
@@ -2,7 +2,11 @@ package com.zw.servicemiya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//服务追踪分析需要暴露的接口
+//在spring Cloud为F版本的时候，已经不需要自己构建Zipkin Server了，只需要下载jar即可，下载地址：
+//https://dl.bintray.com/openzipkin/maven/io/zipkin/java/zipkin-server/
+//下载完成jar 包之后，需要运行jar，如下： java -jar zipkin-server-2.10.1-exec.jar
+//访问浏览器localhost:9494
 @SpringBootApplication
 public class ServiceMiyaApplication {
 
